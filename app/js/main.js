1d2ad35137f1555b11563bd7f6b6b5f1bd48f856
DimaLnS <dmadamchuk@gmail.com>
2022-04-18 14:47:23 +0300

    first slider and cftegory`s list

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 $(function() {
+  $('.first__slider').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
   $('.header-top__select').styler();
   $('.header-bottom__select').styler();
-  $('.header-bar__select').styler();
+ 
 
 });
